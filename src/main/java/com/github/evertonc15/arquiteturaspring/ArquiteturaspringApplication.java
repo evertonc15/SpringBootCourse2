8e89e3f74c7d3f00d55225200c82683db32575d4
evertonc15 <everton.ci@outlook.com>
2025-01-29 08:58:06 -0300

    feat: Entendendo a classe Application

--- src/main/java/com/github/evertonc15/arquiteturaspring/ArquiteturaspringApplication.java
+++ src/main/java/com/github/evertonc15/arquiteturaspring/ArquiteturaspringApplication.java
@@ -1,13 +1,35 @@
 package com.github.evertonc15.arquiteturaspring;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class ArquiteturaspringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ArquiteturaspringApplication.class, args);
+		//SpringApplication.run(ArquiteturaspringApplication.class, args);
+
+		SpringApplicationBuilder builder =
+				new SpringApplicationBuilder(ArquiteturaspringApplication.class);
+
+		builder.bannerMode(Banner.Mode.OFF);
+		builder.profiles("producao", "homologacao");
+
+		builder.run(args);
+
+		//contexto da aplicação já iniciada:
+		ConfigurableApplicationContext applicationContext = builder.context();
+		//var produtoRepository = applicationContext.getBean("produtoRepository");
+
+		ConfigurableEnvironment environment = applicationContext.getEnvironment();
+		String applicantionName = environment.getProperty("spring.application.name");
+		System.out.println("Nome da aplicação: " + applicantionName);
+
 	}
 
 }
