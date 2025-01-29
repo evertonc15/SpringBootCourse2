9f41fe194c35652cccc2e6400cf7894ce0b51c27
evertonc15 <everton.ci@outlook.com>
2025-01-29 08:22:12 -0300

    first commit

---
+++ src/test/java/com/github/evertonc15/arquiteturaspring/ArquiteturaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.evertonc15.arquiteturaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
