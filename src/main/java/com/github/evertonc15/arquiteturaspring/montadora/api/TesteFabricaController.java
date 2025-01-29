21bc6666126d69f11f779d9ea1756cbbb112e0c5
evertonc15 <everton.ci@outlook.com>
2025-01-29 11:01:22 -0300

    feat: Testando o fluxo através do postman

--- src/main/java/com/github/evertonc15/arquiteturaspring/montadora/api/TesteFabricaController.java
+++ src/main/java/com/github/evertonc15/arquiteturaspring/montadora/api/TesteFabricaController.java
@@ -4,9 +4,11 @@ import com.github.evertonc15.arquiteturaspring.montadora.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/carros")
 public class TesteFabricaController {
 
     @Autowired
