661427264836a2ffbed6a2a8b8f720e6e7e9dc64
evertonc15 <everton.ci@outlook.com>
2025-01-29 11:13:56 -0300

    feat: Utilizando Qualifiers

--- src/main/java/com/github/evertonc15/arquiteturaspring/montadora/api/TesteFabricaController.java
+++ src/main/java/com/github/evertonc15/arquiteturaspring/montadora/api/TesteFabricaController.java
@@ -2,6 +2,7 @@ package com.github.evertonc15.arquiteturaspring.montadora.api;
 
 import com.github.evertonc15.arquiteturaspring.montadora.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TesteFabricaController {
 
     @Autowired
+    @Qualifier("motorTurbo")
     private Motor motor;
 
     @PostMapping
@@ -19,4 +21,5 @@ public class TesteFabricaController {
         var carro = new HondaHRV(motor);
         return carro.darIgnicao(chave);
     }
+
 }
