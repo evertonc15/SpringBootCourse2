409b04e7f4356c1739d2dc0725ece6c1dde40508
evertonc15 <everton.ci@outlook.com>
2025-01-30 08:45:02 -0300

    feat: Codificando as camadas da arquitetura completa

---
+++ src/main/java/com/github/evertonc15/arquiteturaspring/todos/TodoService.java
@@ -0,0 +1,17 @@
+package com.github.evertonc15.arquiteturaspring.todos;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public TodoEntity salvar(TodoEntity novoTodo){
+        return todoRepository.save(novoTodo);
+    }
+}
