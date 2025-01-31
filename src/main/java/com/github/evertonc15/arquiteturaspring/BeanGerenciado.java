c7f906eaae11dac39312ce9dd211114f716b9b1a
evertonc15 <everton.ci@outlook.com>
2025-01-31 09:52:32 -0300

    feat: Escopo dos beans @Scope

--- src/main/java/com/github/evertonc15/arquiteturaspring/BeanGerenciado.java
+++ src/main/java/com/github/evertonc15/arquiteturaspring/BeanGerenciado.java
@@ -3,10 +3,21 @@ package com.github.evertonc15.arquiteturaspring;
 import com.github.evertonc15.arquiteturaspring.todos.TodoEntity;
 import com.github.evertonc15.arquiteturaspring.todos.TodoValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
 
+// singleton
 @Component
+@Scope(BeanDefinition.SCOPE_SINGLETON)
+//@Scope(WebApplicationContext.SCOPE_APPLICATION)
+//@Scope("request")
+//@Scope("session")
+//@Scope("application")
 public class BeanGerenciado {
+
+    private String idUsuarioLogado;
 
     @Autowired
     private TodoValidator validator;
